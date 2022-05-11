fbb7a9b2f76fc483ab603394ea47a6b4dd5ebb28
laopengyou <stf30111994@gmail.com>
2022-05-11 08:03:53 +0200

    dkw pere algo funciona

--- pokemon.js
+++ pokemon.js
@@ -10,22 +10,42 @@ const formBuscar = document.getElementById("form");
 
 
 
+
+
+
+//--------------------no funciona------------------------
+const mostrarPokemon = (pokemon) => {
+    pokemones.innerHTML = "";
+    pokemon.forEach((pokemon03) => {
+      pokemones.innerHTML += `
+                  <div class="card col-lg-3 col-xs-12 col-md-6">
+                      <div class="personaje">
+                      <div class="card-body">
+                      <h3 class="card-header">${pokemon03.name}</h3>
+                      <img style="height: 200px; width: 100%; display: block;" src="${pokemon03.url}"  alt="Card image">
+                      </div>
+                      </div>
+                      </div>
+                       `;
+    });
+  };
+//-------------------------------------------------------
+
+
+
 //----------------axios--------------------------------
-async function getPokemon(){
+const getPokemon = async ()=> {
     try {
         const res = await axios.get(API_URL);
-        const pokemon02 =res.data.results;
-        console.log(pokemon02)
+        const pokemon02 = res.data.results;
+        mostrarPokemon(pokemon02)
     } catch (error) {
         console.error(error)
     }
 }
 getPokemon()
+formBuscar.addEventListener("submit",getPokemon);
 //-------------------------------------------------------
-
-
-
-
 
 
 
