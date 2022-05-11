a2ee5340172fa424826be25559a5720a34fd9d91
tianfanshan <stf30111994@gmail.com>
2022-05-11 14:10:14 +0200

    ejercicio medio hecho falta random

--- pokemon.js
+++ pokemon.js
@@ -4,7 +4,8 @@
 //-----------------------constante---------------------
 const API_URL = "https://pokeapi.co/api/v2/pokemon/";
 const pokemones = document.querySelector(".pokemon");
-const formBuscar = document.getElementById("form");
+const botonParaPintar = document.getElementById("botonparapintar");
+const inputBuscar = document.getElementById("buscar")
 //-----------------------------------------------------
 
 
@@ -16,43 +17,19 @@ const formBuscar = document.getElementById("form");
 //--------------------no funciona------------------------
 const mostrarPokemon = (pokemon) => {
     pokemones.innerHTML = "";
-    pokemon.forEach((pokemon03) => {
+    
       pokemones.innerHTML += `
                   <div class="card col-lg-3 col-xs-12 col-md-6">
                       <div class="personaje">
                       <div class="card-body">
-                      <h3 class="card-header">${pokemon03.name}</h3>
-                      <img style="height: 200px; width: 100%; display: block;" src="${pokemon03.url}"  alt="Card image">
+                      <h3 class="card-header">${pokemon.species.name}</h3>
+                      <img style="height: 200px; width: 100%; display: block;" src="${pokemon.sprites.front_default}"  alt="Card image">
                       </div>
                       </div>
                       </div>
                        `;
-    });
   };
 //-------------------------------------------------------
-
-
-
-//----------------axios--------------------------------
-const getPokemon = async ()=> {
-    try {
-        const res = await axios.get(API_URL);
-        const pokemon02 = res.data.results;
-        mostrarPokemon(pokemon02)
-    } catch (error) {
-        console.error(error)
-    }
-}
-getPokemon()
-formBuscar.addEventListener("submit",getPokemon);
-//-------------------------------------------------------
-
-
-
-
-
-
-
 
 
 //-----------Numero aleatorio---------------------------
@@ -60,8 +37,35 @@ const getRandomInt = (min,max) => {
     return Math.floor(Math.random()*(max - min)) + min;
 }
 let random = getRandomInt(1,21)
-console.log(random)
+
 //-------------------------------------------------------
+
+
+//----------------axios--------------------------------
+const getPokemon = async ()=> {
+    try {
+        const pokemonElegido = inputBuscar.value
+        const pokemonRandom = pokemonElegido * random
+        const res = await axios.get(API_URL + pokemonRandom);
+        const pokemon02 =  await res.data;
+        mostrarPokemon(pokemon02)
+    } catch (error) {
+        console.error(error)
+    }
+}
+
+botonParaPintar.addEventListener("click",getPokemon);
+//-------------------------------------------------------
+
+
+
+
+
+
+
+
+
+
 
 
 
